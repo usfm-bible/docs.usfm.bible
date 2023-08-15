aee74edc34ad50488dac103328e14e8af5c71008
klassenjm <klassenjm@gmail.com>
2023-08-14 20:25:22 -0400

    Resolving Uncaught ReferenceError (medium-zoom)

--- supplemental-ui/js/medium-zoom.config.js
+++ supplemental-ui/js/medium-zoom.config.js
@@ -1,4 +1,4 @@
-;(function () {
-  var Mzm = (window.mzm = require('vendor/medium-zoom.min.js'))
-  Mzm('.doc span.frame img, span.zoom img', { background: '#fff' })
-})()
+mediumZoom('.doc img', {
+    background: '#fff'
+  })
+  
--- supplemental-ui/js/vendor/medium-zoom.bundle.js
+++
@@ -1,4 +0,0 @@
-;(function () {
-    var Mzm = (window.mzm = require('medium-zoom/dist/medium-zoom.min'))
-    Mzm('span.frame img, span.zoom img', { background: '#fff' })
-  })()
